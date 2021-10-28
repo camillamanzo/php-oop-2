57c97adaa894526cb064632063930d89b14805c8
camillamanzo <camillasouthafrica@gmail.com>
2021-10-28 16:53:13 +0200

    added class user

--- index.php
+++ index.php
@@ -5,15 +5,44 @@
         protected $name;
         protected $delivery = [];
 
+        // construct function to initialize class
         protected function __construct($name, $delivery){
+
             $this -> name = $name;
             $this -> delivery = $delivery;
         }
 
-        protected function available(){
+        //  function to check if product is available in italy
+        protected function availableInItaly(){
 
             return in_array($italy, $delivery);
         }
+    }
+
+    // initialized class user part of OnlineShop
+    class User extends OnlineShop{
+
+        protected $userName;
+        protected $boughtProducts = [];
+        protected $loyalty = false;
+        protected $card;
+        protected $discount;
+
+
+        protected function __construct($userName, $boughtProducts, $loyalty, $card, $cart, $discount){
+
+            $this -> userName = $userName;
+            $this -> boughtProducts = $boughtProducts;
+            $this -> loyalty = $loyalty;
+            $this -> card = $card;
+            $this -> cart = $cart;
+            $this -> discount = $discount;
+        }
+
+        protected function isLoyal(){
+            return count($boughtProducts) > 10;
+        }
+
     }
 
 ?>
